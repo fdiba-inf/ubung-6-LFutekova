a58b5cdab7ef4e08ebf4f2c8f83d497544dddf72
Lilia Futekova <futekoval@gmail.com>
2020-11-06 09:59:03 +0200

    Ubung 6

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+        boolean palindrome = false;
+
+        assert number > 0;
+
+        int tempNum = number;
+        int digitCounter = 0;
+        int reversedNum = 0;
+
+        while(tempNum != 0){
+            int digit = tempNum % 10;
+            tempNum /= 10;
+            reversedNum += digit * Math.pow(10,digitCounter);
+            digitCounter++;
+        }
+
+        if(number == reversedNum){
+            palindrome = true;
+        }
+
+        System.out.println("Palindrome: " + palindrome);
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -4,6 +4,31 @@ import java.util.Scanner;
 
 public class PasswordCheck {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        boolean noSymbols = true;
+        int digitCounter = 0;
+        do {
+            System.out.print("Enter Password: ");
+            String password = input.next();
+
+            if (password.length() < 8) {
+                continue;
+            }
+
+            for (int i = 0; i < password.length(); i++) {
+                if(!Character.isLetterOrDigit(password.charAt(i))){
+                    noSymbols = false;
+                    break;
+                }
+                if(Character.isDigit(password.charAt(i))){
+                    digitCounter++;
+                }
+            }
+
+        }while(!noSymbols || digitCounter < 2);
+
+        System.out.println("Password valid!");
     }
 }
